7200297a13b504986e3b05a773ac23f67d249f1d
Peter Le Bek <peteralebek@gmail.com>
2016-06-21 00:06:47 -0400

    elm-format

--- src/Cassowary.elm
+++ src/Cassowary.elm
@@ -14,39 +14,40 @@ import Cassowary.Equation exposing (..)
 import Cassowary.Infix exposing (..)
 import Dict
 import Array
-
 import List
 
+
 -- there's a bug in elm's Dict.merge https://github.com/elm-lang/core/pull/648
-merge
-  :  (comparable -> a -> result -> result)
-  -> (comparable -> a -> b -> result -> result)
-  -> (comparable -> b -> result -> result)
-  -> Dict.Dict comparable a
-  -> Dict.Dict comparable b
-  -> result
-  -> result
+
+
+merge :
+    (comparable -> a -> result -> result)
+    -> (comparable -> a -> b -> result -> result)
+    -> (comparable -> b -> result -> result)
+    -> Dict.Dict comparable a
+    -> Dict.Dict comparable b
+    -> result
+    -> result
 merge leftStep bothStep rightStep leftDict rightDict initialResult =
-  let
-    stepState rKey rValue (list, result) =
-      case list of
-        [] ->
-          (list, rightStep rKey rValue result)
+    let
+        stepState rKey rValue ( list, result ) =
+            case list of
+                [] ->
+                    ( list, rightStep rKey rValue result )
 
-        (lKey, lValue) :: rest ->
-          if lKey < rKey then
-            stepState rKey rValue (rest, leftStep lKey lValue result)
+                ( lKey, lValue ) :: rest ->
+                    if lKey < rKey then
+                        stepState rKey rValue ( rest, leftStep lKey lValue result )
+                    else if lKey > rKey then
+                        ( list, rightStep rKey rValue result )
+                    else
+                        ( rest, bothStep lKey lValue rValue result )
 
-          else if lKey > rKey then
-            (list, rightStep rKey rValue result)
+        ( leftovers, intermediateResult ) =
+            Dict.foldl stepState ( Dict.toList leftDict, initialResult ) rightDict
+    in
+        List.foldl (\( k, v ) result -> leftStep k v result) intermediateResult leftovers
 
-          else
-            (rest, bothStep lKey lValue rValue result)
-
-    (leftovers, intermediateResult) =
-      Dict.foldl stepState (Dict.toList leftDict, initialResult) rightDict
-  in
-    List.foldl (\(k,v) result -> leftStep k v result) intermediateResult leftovers
 
 {-| EquationStdForm is a phantom data type. Don't instantiate it yourself, use
 standardForm instead.
@@ -55,20 +56,30 @@ type EquationStdForm
     = EqualityStdForm Expression
     | InequalityStdForm Expression
 
-type alias BasicRows = Dict.Dict String EquationStdForm
-type alias NonBasicRows = Array.Array EquationStdForm
 
-type alias Tableau = {
-    basic: BasicRows
-  , nonBasic: NonBasicRows
-}
+type alias BasicRows =
+    Dict.Dict String EquationStdForm
+
+
+type alias NonBasicRows =
+    Array.Array EquationStdForm
+
+
+type alias Tableau =
+    { basic : BasicRows
+    , nonBasic : NonBasicRows
+    }
 
 
 isEmpty : Dict.Dict comparable b -> Bool
 isEmpty =
     List.isEmpty << Dict.keys
 
+
+
 -- eliminates terms where coeff = 0
+
+
 addDicts : Dict.Dict comparable number -> Dict.Dict comparable number -> Dict.Dict comparable number
 addDicts a b =
     let
@@ -82,12 +93,17 @@ addDicts a b =
             Dict.insert k v2 acc
     in
         merge c1 c2 c3 a b Dict.empty
-          |> Dict.filter (\k v -> v /= 0)
+            |> Dict.filter (\k v -> v /= 0)
+
 
 subtractDicts : Dict.Dict comparable number -> Dict.Dict comparable number -> Dict.Dict comparable number
-subtractDicts a b = addDicts a <| Dict.map (\k v -> negate v) b
+subtractDicts a b =
+    addDicts a <| Dict.map (\k v -> negate v) b
+
+
 
 -- vars on one side, consts on the other
+
 
 rebalance : Equation -> Equation
 rebalance expr =
@@ -136,8 +152,9 @@ standardForm expr =
 
 -- SIMPLEX
 -- minimum negative objective coefficient, else Nothing
-
 -- get the entering basic var (if any)
+
+
 getEnteringVar : VariableMap -> Maybe String
 getEnteringVar objective =
     let
@@ -167,6 +184,7 @@ getLeavingVar tableau enteringVar =
     let
         ratios =
             Array.indexedMap (\i row -> ( i, blandRatio enteringVar row )) tableau
+
         f ( idx, ratio ) acc =
             case ( ratio, acc ) of
                 ( Just ratio, Just ( accI, accR ) ) ->
@@ -201,6 +219,7 @@ nonposToNothing n =
 -- (pivot coefficient) / (row constant)
 -- row constant must be strictly positive, else Nothing
 -- aka Minumum Ratio Test (MRT)
+
 
 blandRatio : String -> EquationStdForm -> Maybe Float
 blandRatio var row =
@@ -243,98 +262,155 @@ divideRowByPivot pivotVar row =
             InequalityStdForm expr ->
                 InequalityStdForm <| refactor expr
 
-getVars stdForm = case stdForm of
-    EqualityStdForm expr -> expr.vars
-    InequalityStdForm expr -> expr.vars
 
-getConst stdForm = case stdForm of
-    EqualityStdForm expr -> expr.const
-    InequalityStdForm expr -> expr.const
+getVars stdForm =
+    case stdForm of
+        EqualityStdForm expr ->
+            expr.vars
 
-mapStdForm f stdForm = case stdForm of
-    EqualityStdForm expr -> EqualityStdForm (f expr)
-    InequalityStdForm expr -> InequalityStdForm (f expr)
+        InequalityStdForm expr ->
+            expr.vars
 
-removeVar v stdForm = mapStdForm (\eq -> {
-    vars = Dict.remove v eq.vars
-  , const = eq.const }) stdForm
 
-type alias Pivot = {
-    enteringVar: String
-  , leavingIdx: Int
-  , leavingRow: EquationStdForm
-}
+getConst stdForm =
+    case stdForm of
+        EqualityStdForm expr ->
+            expr.const
 
-andThen' = flip Maybe.andThen
+        InequalityStdForm expr ->
+            expr.const
+
+
+mapStdForm f stdForm =
+    case stdForm of
+        EqualityStdForm expr ->
+            EqualityStdForm (f expr)
+
+        InequalityStdForm expr ->
+            InequalityStdForm (f expr)
+
+
+removeVar v stdForm =
+    mapStdForm
+        (\eq ->
+            { vars = Dict.remove v eq.vars
+            , const = eq.const
+            }
+        )
+        stdForm
+
+
+type alias Pivot =
+    { enteringVar : String
+    , leavingIdx : Int
+    , leavingRow : EquationStdForm
+    }
+
+
+andThen' =
+    flip Maybe.andThen
+
 
 getPivot : EquationStdForm -> BasicRows -> NonBasicRows -> Maybe Pivot
 getPivot objective basicRows nonBasicRows =
-  let
-    both = (Array.append nonBasicRows <| Array.fromList <| Dict.values basicRows)
-  in
-  (getEnteringVar <| getVars objective)
-    |> andThen' (\enteringVar -> getLeavingVar both enteringVar
-      |> andThen' (\leavingIdx ->  Array.get leavingIdx both
-        |> Maybe.map (\leavingRow -> {
-          enteringVar=enteringVar, leavingIdx=leavingIdx, leavingRow=leavingRow
-        })))
+    let
+        both =
+            (Array.append nonBasicRows <| Array.fromList <| Dict.values basicRows)
+    in
+        (getEnteringVar <| getVars objective)
+            |> andThen'
+                (\enteringVar ->
+                    getLeavingVar both enteringVar
+                        |> andThen'
+                            (\leavingIdx ->
+                                Array.get leavingIdx both
+                                    |> Maybe.map
+                                        (\leavingRow ->
+                                            { enteringVar = enteringVar
+                                            , leavingIdx = leavingIdx
+                                            , leavingRow = leavingRow
+                                            }
+                                        )
+                            )
+                )
 
 
 substituteEquation : Pivot -> EquationStdForm -> EquationStdForm -> EquationStdForm
 substituteEquation pivot replacement equation =
-  case Dict.get pivot.enteringVar (getVars equation) of
-    Just coeff ->
-      let
-          scaled = mapStdForm (\e -> {
-            vars = Dict.map (\k v -> v*coeff) e.vars
-          , const = e.const*coeff
-          }) replacement
-      in
-          mapStdForm (\e -> {
-            vars = subtractDicts e.vars (getVars scaled)
-          , const = e.const - (getConst scaled)
-          }) equation
+    case Dict.get pivot.enteringVar (getVars equation) of
+        Just coeff ->
+            let
+                scaled =
+                    mapStdForm
+                        (\e ->
+                            { vars = Dict.map (\k v -> v * coeff) e.vars
+                            , const = e.const * coeff
+                            }
+                        )
+                        replacement
+            in
+                mapStdForm
+                    (\e ->
+                        { vars = subtractDicts e.vars (getVars scaled)
+                        , const = e.const - (getConst scaled)
+                        }
+                    )
+                    equation
 
-
-    Nothing ->
-      equation
+        Nothing ->
+            equation
 
 
 substituteRows : Pivot -> EquationStdForm -> NonBasicRows -> NonBasicRows
 substituteRows pivot replacement nonBasicRows =
     Array.map (substituteEquation pivot replacement) nonBasicRows
 
+
 substituteObjective : Pivot -> EquationStdForm -> EquationStdForm -> EquationStdForm
 substituteObjective pivot replacement objective =
-  substituteEquation pivot replacement objective
+    substituteEquation pivot replacement objective
+
 
 arrayRemove : Int -> Array.Array a -> Array.Array a
 arrayRemove idx array =
-  Array.append
-    (Array.slice 0 idx array)
-    (Array.slice (idx+1) ((Array.length array)-1) array)
+    Array.append (Array.slice 0 idx array)
+        (Array.slice (idx + 1) ((Array.length array) - 1) array)
+
+
 
 -- TODO pivot should move the new basic to a separate map
 -- so prob need to change Tableau data structure
 
+
 {-| Perform a pivot - transforms Tableau to new canonical form
 -}
-doPivot : EquationStdForm -> Tableau -> Maybe (Tableau, EquationStdForm)
+doPivot : EquationStdForm -> Tableau -> Maybe ( Tableau, EquationStdForm )
 doPivot objective tableau =
-  let
-    pivot = getPivot objective tableau.basic tableau.nonBasic
-    getReplacement pivot = divideRowByPivot pivot.enteringVar pivot.leavingRow
-    replace pivot replacement = ({
-          basic=Dict.insert pivot.enteringVar (removeVar pivot.enteringVar replacement) <| Dict.map (\k v -> substituteEquation pivot replacement v) tableau.basic
-        , nonBasic=substituteRows pivot replacement (arrayRemove pivot.leavingIdx tableau.nonBasic)
-      }
-      , substituteObjective pivot replacement objective
-    )
-  in pivot
-    |> Maybe.map (\pivot -> getReplacement pivot |> replace pivot)
+    let
+        pivot =
+            getPivot objective tableau.basic tableau.nonBasic
 
-{-| Pptimizes the given tableau -}
-simplex : EquationStdForm -> Tableau -> (Tableau, EquationStdForm)
-simplex objective tableau = case doPivot objective tableau of
-  Just (tableau, objective) -> simplex objective tableau
-  Nothing -> (tableau, objective)
+        getReplacement pivot =
+            divideRowByPivot pivot.enteringVar pivot.leavingRow
+
+        replace pivot replacement =
+            ( { basic = Dict.insert pivot.enteringVar (removeVar pivot.enteringVar replacement) <| Dict.map (\k v -> substituteEquation pivot replacement v) tableau.basic
+              , nonBasic = substituteRows pivot replacement (arrayRemove pivot.leavingIdx tableau.nonBasic)
+              }
+            , substituteObjective pivot replacement objective
+            )
+    in
+        pivot
+            |> Maybe.map (\pivot -> getReplacement pivot |> replace pivot)
+
+
+{-| Pptimizes the given tableau
+-}
+simplex : EquationStdForm -> Tableau -> ( Tableau, EquationStdForm )
+simplex objective tableau =
+    case doPivot objective tableau of
+        Just ( tableau, objective ) ->
+            simplex objective tableau
+
+        Nothing ->
+            ( tableau, objective )
--- test/Tests.elm
+++ test/Tests.elm
@@ -4,14 +4,14 @@ import ElmTest exposing (..)
 import Cassowary.Equation exposing (..)
 import Cassowary.Infix exposing (..)
 import Cassowary exposing (..)
-
 import Dict
 import Array
+
 
 astSuite : Test
 astSuite =
     suite "AST tests"
-        [ test "Simple" <|  (Coeff (Var "x") 2) `assertEqual` (Var "x" .*. 2) ]
+        [ test "Simple" <| (Coeff (Var "x") 2) `assertEqual` (Var "x" .*. 2) ]
 
 
 assertExpressionsEqual expr1 expr2 =
@@ -20,6 +20,7 @@ assertExpressionsEqual expr1 expr2 =
             Dict.toList << .vars
     in
         assert ((getVars expr1) == (getVars expr2) && expr1.const == expr2.const)
+
 
 makeExpressionSuite : Test
 makeExpressionSuite =
@@ -35,35 +36,49 @@ makeExpressionSuite =
                     }
             ]
 
-makeProgram constraints objective = ({
-    basic=Dict.empty
-  , nonBasic=(Array.fromList <| List.map standardForm constraints)
-  }, standardForm <| objective)
+
+makeProgram constraints objective =
+    ( { basic = Dict.empty
+      , nonBasic = (Array.fromList <| List.map standardForm constraints)
+      }
+    , standardForm <| objective
+    )
+
+
 
 -- Ex 2.1 Linear Programming: Foundations and Extensions
 -- (x1,x2,x3,x4) = (2,0,1,0), ζ = 17.
-problem1 = makeProgram [
-    Var "x1" .*. 2 .+. Var "x2" .+. Var "x3" .+. Var "x4" .*. 3 .<=. Lit 5
-  , Var "x1" .+. Var "x2" .*. 3 .+. Var "x3" .+. Var "x4" .*. 2 .<=. Lit 3
-  ]
-  (Var "p" .=. Var "x1" .*. 6 .+. Var "x2" .*. 8 .+. Var "x3" .*. 5 .+. Var "x4" .*. 9)
+
+
+problem1 =
+    makeProgram
+        [ Var "x1" .*. 2 .+. Var "x2" .+. Var "x3" .+. Var "x4" .*. 3 .<=. Lit 5
+        , Var "x1" .+. Var "x2" .*. 3 .+. Var "x3" .+. Var "x4" .*. 2 .<=. Lit 3
+        ]
+        (Var "p" .=. Var "x1" .*. 6 .+. Var "x2" .*. 8 .+. Var "x3" .*. 5 .+. Var "x4" .*. 9)
+
 
 
 -- Ex 2.2 Linear Programming: Foundations and Extensions
 -- (x1,x2) = (1,0), ζ = 2.
-problem2 = makeProgram [
-    Var "x1" .*. 2 .+. Var "x2" .<=. Lit 4
-  , Var "x1" .*. 2 .+. Var "x2" .*. 3 .<=. Lit 3
-  , Var "x1" .*. 4 .+. Var "x2" .<=. Lit 5
-  , Var "x1" .+. Var "x2" .*. 5 .<=. Lit 1
-  ]
-  (Var "p" .=. Var "x1" .*. 2 .+. Var "x2" .*. 8)
+
+
+problem2 =
+    makeProgram
+        [ Var "x1" .*. 2 .+. Var "x2" .<=. Lit 4
+        , Var "x1" .*. 2 .+. Var "x2" .*. 3 .<=. Lit 3
+        , Var "x1" .*. 4 .+. Var "x2" .<=. Lit 5
+        , Var "x1" .+. Var "x2" .*. 5 .<=. Lit 1
+        ]
+        (Var "p" .=. Var "x1" .*. 2 .+. Var "x2" .*. 8)
+
 
 pivotSuite : Test
-pivotSuite = suite "Pivot tests"
-    [
-      test "Simple" <| problem2 `assertEqual` (simplex (snd problem2) (fst problem2))
-    ]
+pivotSuite =
+    suite "Pivot tests"
+        [ test "Simple" <| problem2 `assertEqual` (simplex (snd problem2) (fst problem2))
+        ]
+
 
 all : Test
 all =
